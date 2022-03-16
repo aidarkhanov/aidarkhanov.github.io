55752f4e509ce0ff5e4172381527a44e1d3ae489
Dair Aidarkhanov <dairaidarkhanov@gmail.com>
2022-03-16 04:30:21 +0200

    Minify css in console log

--- src/scripts/modules/easter-egg.js
+++ src/scripts/modules/easter-egg.js
@@ -1,4 +1,4 @@
 console.log("%cHey, dear recruiters!", "color:#ff4800;font-size:24px")
-console.log("%cThank you for taking a peek at the console tab.", "font-size: 20px")
-console.log("%cFor more, visit my GitHub page: https://github.com/aidarkhanov/", "font-size: 20px")
+console.log("%cThank you for taking a peek at the console tab.", "font-size:20px")
+console.log("%cFor more, visit my GitHub page: https://github.com/aidarkhanov/", "font-size:20px")
 console.log("%c+", "color:transparent;font-size:1px;padding:180px 320px;background-image:url(https://media3.giphy.com/media/gFDewb47lwKrYEtKKn/giphy.gif);background-size:640px 370px")
