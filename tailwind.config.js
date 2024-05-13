39b64cb788b0355882097291ea7e025342646ac0
aidarkhanov <dairaidarkhanov@gmail.com>
2024-05-13 23:49:33 +0000

    formatting and gitignore update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import plugin from 'tailwindcss/plugin';
+import defaultTheme from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+
   theme: {
     extend: {
       fontFamily: {
         sans: [
-          ['InterVariable', ...defaultTheme.fontFamily.sans],
+          ["InterVariable", ...defaultTheme.fontFamily.sans],
           {
             fontFeatureSettings: '"liga", "calt", "dlig", "ss02", "ss03"',
             fontVariationSettings: '"opsz" 32',
@@ -17,23 +18,24 @@ export default {
       },
     },
   },
+
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/container-queries'),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
 
     plugin(({ addUtilities, theme, e }) => {
       const newUtilities = {};
       const shorthand = {
-        'marginBlockStart': 'mbs',
-        'marginBlockEnd': 'mbe',
-        'paddingBlockStart': 'pbs',
-        'paddingBlockEnd': 'pbe',
+        marginBlockStart: "mbs",
+        marginBlockEnd: "mbe",
+        paddingBlockStart: "pbs",
+        paddingBlockEnd: "pbe",
       };
 
-      Object.keys(shorthand).forEach(key => {
-        const sizeVariants = theme('spacing');
+      Object.keys(shorthand).forEach((key) => {
+        const sizeVariants = theme("spacing");
         for (let size in sizeVariants) {
           newUtilities[`.${e(`${shorthand[key]}-${size}`)}`] = {
             [key]: sizeVariants[size],
@@ -41,7 +43,7 @@ export default {
         }
       });
 
-      addUtilities(newUtilities, ['responsive', 'hover']);
+      addUtilities(newUtilities, ["responsive", "hover"]);
     }),
   ],
 };
